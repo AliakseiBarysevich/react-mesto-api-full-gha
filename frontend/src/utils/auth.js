9aa0d4a675b6f399763cbcc8cf672dba11ce73fa
Aliaksei Barysevich <barysevichaliaksei@yandex.by>
2023-04-08 01:59:17 +0300

    feat: README updated

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "api.mestoapp.nomoredomains.monster";
+export const BASE_URL = "https://api.mestoapp.nomoredomains.monster";
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
